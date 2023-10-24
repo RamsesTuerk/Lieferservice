4c554104a90b39f669ac7c515c0343115ff544bf
Max <maxwiese15.1@gmx.de>
2023-10-24 19:51:08 +0200

    Scrip Update, Function zum Auslesen der Burger.csv erstellt

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-// Hier wird der JavaScript Code für das Einlesen und Anzeigen von CSV-Daten implementiert.
-var csvFile = "/Datenbanken/burger_restaurants.csv";
+function loadCSV() {
+    var csvFile = "/Datenbanken/burger_restaurants.csv";
+    var table = document.getElementById("csvTable");
+    var xhttp = new XMLHttpRequest();
 
-var table = document.getElementById("csvTable");
-var xhttp = new XMLHttpRequest();
-
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        var lines = this.responseText.split("\n");
-        for (var i = 0; i < lines.length; i++) {
-            var row = table.insertRow(-1);
-            var cells = lines[i].split(",");
-            for (var j = 0; j < cells.length; j++) {
-                var cell = row.insertCell(-1);
-                cell.innerHTML = cells[j];
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                var lines = this.responseText.split("\n");
+                for (var i = 0; i < lines.length; i++) {
+                    var row = table.insertRow(-1);
+                    var cells = lines[i].split(",");
+                    for (var j = 0; j < cells.length; j++) {
+                        var cell = row.insertCell(-1);
+                        cell.innerHTML = cells[j];
+                    }
+                }
+            } else {
+                console.error("Failed to load CSV. Status code: " + this.status);
             }
+        }else {
+            console.error("Failed to load CSV. Status code: " + this.status);
         }
-    }
-};
+    };
 
-xhttp.open("GET", csvFile, true);
-xhttp.send();
+    xhttp.open("GET", csvFile, true);
+    xhttp.send();
+}
