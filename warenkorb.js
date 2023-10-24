5833f20f9d8fbe34981c4fbcee059b413873ad6b
RamsesTuerk <ramses.tuerk@iu-study.org>
2023-10-24 20:40:57 +0200

    Funktionierender Prototyp eines Warenkorbs

--- warenkorb.js
+++ warenkorb.js
@@ -1,1 +1,25 @@
 //Dieser Code wird dazu verwendet einen Warenkorb funktionstechnisch Darzustellen 
+var warenkorb = [];
+
+function zumWarenkorb(nr) {
+    var produkt = document.getElementById('produkt' + nr).innerHTML;
+    warenkorb.push(produkt);
+    warenkorbAusgeben();
+}
+
+function warenkorbAusgeben() {
+    var ausgabe = '';
+        for (var i = 0; i < warenkorb.length; ++i) {
+            warenkorb.sort();
+            ausgabe += '<li id="nr' + i + '">';
+            ausgabe += warenkorb[i];
+            ausgabe += '<input type="button" value="X" id="loeschen" onclick="loescheNr(' + i + ');" />';
+            ausgabe += '</li>';
+    }
+    document.getElementById('waren').innerHTML = ausgabe;
+}
+
+function loescheNr(id) {
+    warenkorb.splice(id, 1);
+    warenkorbAusgeben();
+}
