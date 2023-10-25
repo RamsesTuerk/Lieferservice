fee4c1df3731857c6a2324a0b331188a9e45f42c
RamsesTuerk <148867726+RamsesTuerk@users.noreply.github.com>
2023-10-25 22:53:20 +0200

    Fix warenkorb.js

--- warenkorb.js
+++ warenkorb.js
@@ -1,51 +1,60 @@
-warenkorb = [];
+// Dieser Code wird dazu verwendet, einen Warenkorb funktional darzustellen.
+var warenkorbPreis = 0;
+var warenkorb = {
+  produkte: [],  // Array für die Namen der Produkte im Warenkorb
+  menge: [],     // Array für die Menge jedes Produkts im Warenkorb
+  preis: []      // Array für den Preis jedes Produkts im Warenkorb
+};
 
 function zumWarenkorb(nr, pr) {
   var produktName = document.getElementById('produkt' + nr).innerHTML;
   var preis = pr;
 
   // Sucht nach dem Produkt im Warenkorb
-  var existingProduct = warenkorb.find(item => item.name === produktName);
+  var index = warenkorb.produkte.indexOf(produktName);
 
-  if (existingProduct) {
-    existingProduct.quantity += 1;
+  if (index === -1) {
+    warenkorb.produkte.push(produktName);
+    warenkorb.menge.push(1);
+    warenkorb.preis.push(preis);
   } else {
-    warenkorb.push({ name: produktName, price: preis, quantity: 1 });
+    warenkorb.menge[index] += 1;
   }
 
   warenkorbAusgeben();
 }
 
 function warenkorbAusgeben() {
+  warenkorbSum();  // Warenkorb-Gesamtpreis berechnen
   var ausgabe = '';
 
-  warenkorbSum();
-  
-  for (var i = 0; i < warenkorb.length; i++) {
-    var item = warenkorb[i];
+  for (var i = 0; i < warenkorb.produkte.length; i++) {
     ausgabe += '<li>';
-    ausgabe += item.quantity + "x " + item.name + ": " + (item.price / 100) + "€ ";
+    ausgabe += warenkorb.menge[i] + "x " + warenkorb.produkte[i] + ": " + (warenkorb.preis[i] / 100) + "€ ";
     ausgabe += '<input type="button" value="X" onclick="loescheProdukt(' + i + ');" />';
     ausgabe += '</li><br>';
   }
 
-  ausgabe += (warenkorbPreis / 100) + "€"
+  ausgabe += (warenkorbPreis / 100) + "€";
 
   document.getElementById('waren').innerHTML = ausgabe;
 }
 
-function warenkorbSum() {
-  warenkorbPreis = warenkorb.reduce((total, item) => total + item.quantity * item.price, 0);
+var warenkorbPreis = 0;
+
+// Schleife durch alle Produkte im Warenkorb
+for (var i = 0; i < warenkorb.produkte.length; i++) {
+  // Gesamtpreis berechnen, indem Menge und Preis multipliziert und zur Gesamtsumme hinzugefügt werden
+  warenkorbPreis += warenkorb.menge[i] * warenkorb.preis[i];
 }
 
 function loescheProdukt(index) {
-  var item = warenkorb[index];
-
-  if (item.quantity > 1) {
-    item.quantity -= 1;
+  if (warenkorb.menge[index] > 1) {
+    warenkorb.menge[index] -= 1;
   } else {
-    warenkorb.splice(index, 1);
+    warenkorb.produkte.splice(index, 1);
+    warenkorb.menge.splice(index, 1);
+    warenkorb.preis.splice(index, 1);
   }
-
   warenkorbAusgeben();
 }
