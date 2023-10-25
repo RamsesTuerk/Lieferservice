7a7f1d14ba11693605f2cd081c7b5e2d1cf3d3f6
RamsesTuerk <ramses.tuerk@iu-study.org>
2023-10-25 22:43:18 +0200

    auskommentiert

--- warenkorb.js
+++ warenkorb.js
@@ -1,70 +1,51 @@
-//Dieser Code wird dazu verwendet einen Warenkorb funktionstechnisch Darzustellen 
-var warenkorbPreis = 0;
-var warenkorb = {
-  produkte: [],
-  menge: [],
-  preis: []
-};
+warenkorb = [];
 
 function zumWarenkorb(nr, pr) {
   var produktName = document.getElementById('produkt' + nr).innerHTML;
   var preis = pr;
 
   // Sucht nach dem Produkt im Warenkorb
-  var index = warenkorb.produkte.indexOf(produktName);
+  var existingProduct = warenkorb.find(item => item.name === produktName);
 
-  if (index === -1) {
-      warenkorb.produkte.push(produktName);
-      warenkorb.menge.push(1);
-      warenkorb.preis.push(preis);
+  if (existingProduct) {
+    existingProduct.quantity += 1;
   } else {
-      warenkorb.menge[index] += 1;
+    warenkorb.push({ name: produktName, price: preis, quantity: 1 });
   }
 
   warenkorbAusgeben();
 }
 
 function warenkorbAusgeben() {
+  var ausgabe = '';
 
-    warenkorbSum()
-    var ausgabe = '';
-
-    for (var i = 0; i < warenkorb.produkte.length; i++) {
-        ausgabe += '<li>';
-        ausgabe += warenkorb.menge[i] + "x " + warenkorb.produkte[i] + ": " + (warenkorb.preis[i] / 100) + "€ ";
-        ausgabe += '<input type="button" value="X" onclick="loescheProdukt(' + i + ');" />';
-        ausgabe += '</li><br>';
-    }
-
-    ausgabe += (warenkorbPreis / 100) + "€"
-
-    document.getElementById('waren').innerHTML = ausgabe;
-
-    console.log(warenkorb.produkte);
-    console.log(warenkorb.menge);
-    console.log(warenkorb.preis);
-    console.log(warenkorbPreis);
-}
-
-function warenkorbSum(){
-  warenkorbPreis = 0;
-
-  for (var i = 0; i < warenkorb.produkte.length; i++) {
-       warenkorbPreis += warenkorb.menge[i] * warenkorb.preis[i];
-  }
-}
-
-function loescheProdukt(index){
+  warenkorbSum();
   
-  if(warenkorb.menge[index] > 1){
-    warenkorb.menge[index] -= 1;
-  }else{
-    warenkorb.produkte.splice(index, 1);
-    warenkorb.menge.splice(index, 1);
-    warenkorb.preis.splice(index, 1);
+  for (var i = 0; i < warenkorb.length; i++) {
+    var item = warenkorb[i];
+    ausgabe += '<li>';
+    ausgabe += item.quantity + "x " + item.name + ": " + (item.price / 100) + "€ ";
+    ausgabe += '<input type="button" value="X" onclick="loescheProdukt(' + i + ');" />';
+    ausgabe += '</li><br>';
   }
-    warenkorbAusgeben();
+
+  ausgabe += (warenkorbPreis / 100) + "€"
+
+  document.getElementById('waren').innerHTML = ausgabe;
 }
 
+function warenkorbSum() {
+  warenkorbPreis = warenkorb.reduce((total, item) => total + item.quantity * item.price, 0);
+}
 
+function loescheProdukt(index) {
+  var item = warenkorb[index];
 
+  if (item.quantity > 1) {
+    item.quantity -= 1;
+  } else {
+    warenkorb.splice(index, 1);
+  }
+
+  warenkorbAusgeben();
+}
