94e56fd3a39a0526754feaf6f98c33f5976a4b0c
RamsesTuerk <ramses.tuerk@iu-study.org>
2023-10-25 14:28:54 +0200

    Warenkorb schlanker und cleaner gestaltet

--- warenkorb.js
+++ warenkorb.js
@@ -1,58 +1,51 @@
 //Dieser Code wird dazu verwendet einen Warenkorb funktionstechnisch Darzustellen 
-var warenkorb;
-var preis;
+var warenkorb = {
+  produkte: [],
+  menge: [],
+  preis: []
+};
 var warenkorbPreis = 0;
-var warenkorbProdukte = [];
-var warenkorbMenge = [];
-var produktPreis = [];
-var ausgabe = '';
 
 function zumWarenkorb(nr, pr) {
-    warenkorb = document.getElementById('produkt' + nr).innerHTML;
-    preis = pr
-    warenkorbAusgeben();
+  var produktName = document.getElementById('produkt' + nr).innerHTML;
+  var preis = pr;
+
+  // Sucht nach dem Produkt im Warenkorb
+  var index = warenkorb.produkte.indexOf(produktName);
+
+  if (index === -1) {
+      warenkorb.produkte.push(produktName);
+      warenkorb.menge.push(1);
+      warenkorb.preis.push(preis);
+  } else {
+      warenkorb.menge[index] += 1;
+  }
+
+  warenkorbAusgeben();
 }
+
 
 
 
 function warenkorbAusgeben() {
-    var isInWarenkorbProdukte = (element) => element == warenkorb;
-    var index = warenkorbProdukte.findIndex(isInWarenkorbProdukte);
-
-    if(index == -1){
-        warenkorbProdukte.push(warenkorb);
-        warenkorbMenge.push(1);
-        produktPreis.push(preis);     
-    }else{
-        warenkorbMenge[index] += 1;
-    }
 
     warenkorbSum()
-    console.log(warenkorbProdukte);
-    console.log(warenkorbMenge);
-    console.log(produktPreis);
+    console.log(warenkorb.produkte);
+    console.log(warenkorb.menge);
+    console.log(warenkorb.preis);
     console.log(warenkorbPreis);
-    warenkorbPreis = 0;
+    
    
-      // for (var j = 0; j < warenkorbProdukte.length; ++j) {
-        //    ausgabe += '<li id="nr' + j + '">';
-          //  ausgabe += warenkorbProdukte[j]  + warenkorbMenge[j];
-            //ausgabe += '<input type="button" value="X" id="loeschen" onclick="loescheNr(' + j + ');" />';
-            //ausgabe += '</li>';
-   // }
 
-   // document.getElementById('waren').innerHTML = ausgabe;
 }
 
 function warenkorbSum(){
-  for (var i = 0; i < warenkorbMenge.length;++i){
-    warenkorbPreis += warenkorbMenge[i] * produktPreis[i] 
+  warenkorbPreis = 0;
+
+  for (var i = 0; i < warenkorb.produkte.length; i++) {
+       warenkorbPreis += warenkorb.menge[i] * warenkorb.preis[i];
   }
 }
 
 
-//function loescheNr(id) {
-//    warenkorb.splice(id, 1);
-//    warenkorbAusgeben();
-//}
 
