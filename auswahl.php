e614b1620fe8ba3ace1bdc60883e08ad43da2aaa
Max <maxwiese15.1@gmx.de>
2023-11-13 18:23:26 +0100

    Update restaurant selection page (generated by useblackbox.ai )

--- auswahl.php
+++ auswahl.php
@@ -11,36 +11,37 @@
             <div class="">
                 <h1>Die für dich passende Auswahl an Restaurants</h1>
             </div>
-            <div class="table">
-                <?php //Beginn des PHP Codes
-                    $servername = "localhost";  // Der Datenbankserver 
-                    $username = "root";  //Datenbank-Benutzername
-                    $password = "";  //Datenbank-Passwort
-                    $database = "restaurants";  //Name der Datenbank
-                    
-                    // Verbindung zur Datenbank herstellen
-                    $connection = new mysqli($servername, $username, $password, $database);
-                    
-                    // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
-                    if ($connection->connect_error) {
-                        die("Verbindung zur Datenbank fehlgeschlagen: " . $connection->connect_error);
-                    }
-                    $kategorie = $_GET["kategorie"]; // Setzt die Variable $kategorie auf den durch die URL übergebenen Wert der Kategorie
-                    $typ = $_GET["typ"]; // Setzt die Variable $typ auf den durch die URL übergebenen Wert des Typen
-                    $preis = $_GET["preis"];// Setzt die Variable $preis auf den durch die URL übergebenen Wert des Preises
+            <div class="container">
+                <div class="table_div container">
+                    <?php //Beginn des PHP Codes
+                        $servername = "localhost";  // Der Datenbankserver 
+                        $username = "root";  //Datenbank-Benutzername
+                        $password = "";  //Datenbank-Passwort
+                        $database = "restaurants";  //Name der Datenbank
+                        
+                        // Verbindung zur Datenbank herstellen
+                        $connection = new mysqli($servername, $username, $password, $database);
+                        
+                        // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
+                        if ($connection->connect_error) {
+                            die("Verbindung zur Datenbank fehlgeschlagen: " . $connection->connect_error);
+                        }
+                        $kategorie = $_GET["kategorie"]; // Setzt die Variable $kategorie auf den durch die URL übergebenen Wert der Kategorie
+                        $typ = $_GET["typ"]; // Setzt die Variable $typ auf den durch die URL übergebenen Wert des Typen
+                        $preis = $_GET["preis"];// Setzt die Variable $preis auf den durch die URL übergebenen Wert des Preises
 
-                    $sql = "SELECT * FROM restaurants_all WHERE Kategorie='$kategorie'AND Typ='$typ'AND Preis='$preis'";
-                    // Die Abfrage ausführen und das Ergebnis speichern
-                    $result = $connection->query($sql);
+                        $sql = "SELECT * FROM restaurants_all WHERE Kategorie='$kategorie'AND Typ='$typ'AND Preis='$preis'";
+                        // Die Abfrage ausführen und das Ergebnis speichern
+                        $result = $connection->query($sql);
 
-                    // Überprüfen, ob die Abfrage erfolgreich war
-                    if ($result) {
-                        // Daten der Db als Tabelle ausgeben und auf der Webseite anzeigen
-                        while ($row = $result->fetch_assoc()) {
-                            echo "<div class='table_div'>";
+                        // Überprüfen, ob die Abfrage erfolgreich war
+                        if ($result) {
+                            // Daten der Db als Tabelle ausgeben und auf der Webseite anzeigen
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<div class='choice_div'>";
                                 echo "<table class='choice_table'>";
                                 echo "<tr>";
-                                echo "<a href='google.com'>"."<td class='td restaurant_name'>"."Restaurant: " . $row["Restaurant_Name"]."</td>"."</a>";
+                                echo "<td class='td restaurant_name'>"."Restaurant: " ."<a href='LukasGRIDTEST.html?Restaurant=$row[Restaurant_Name]'>". $row["Restaurant_Name"]."</a>"."</td>";
                                 echo "</tr>";
                                 echo "<tr>";
                                 echo "<td class='td restaurant_min_order'>"."Mindestbestellwert: ". $row["Mindestbestellwert"]. " €"."</td>";
@@ -55,15 +56,16 @@
                                 echo "<td class='td restaurant_price'>"."Preis: " . $row["Preis"]."</td>"."</br>";
                                 echo "</tr>";
                                 echo "</table>";
-                            echo "</div>";
-                        }
+                                echo "</div>";
+                            }
 
-                        // Verbindung schließen
-                        $connection->close();
-                    } else {
-                        echo "Fehler bei der Abfrage: " . $connection->error;
-                    }
-                ?> <!--Ende des PHP Codes-->
+                            // Verbindung schließen
+                            $connection->close();
+                        } else {
+                            echo "Fehler bei der Abfrage: " . $connection->error;
+                        }
+                    ?> <!--Ende des PHP Codes-->
+                </div>
             </div>
         </div>
     </body>
--- auswahl_skip.php
+++ auswahl_skip.php
@@ -11,57 +11,59 @@
             <div class="">
                 <h1>Die für dich passende Auswahl an Restaurants</h1>
             </div>
-            <div class="table">
-            <?php
-                $servername = "localhost";  // Der Datenbankserver 
-                $username = "root";  //Datenbank-Benutzername
-                $password = "";  //Datenbank-Passwort
-                $database = "restaurants";  //Name der Datenbank
-                
-                // Verbindung zur Datenbank herstellen
-                $connection = new mysqli($servername, $username, $password, $database);
-                
-                // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
-                if ($connection->connect_error) {
-                    die("Verbindung zur Datenbank fehlgeschlagen: " . $connection->connect_error);
-                }
+            <div class="container">
+                <div class="table_div container">
+                    <?php
+                        $servername = "localhost";  // Der Datenbankserver 
+                        $username = "root";  //Datenbank-Benutzername
+                        $password = "";  //Datenbank-Passwort
+                        $database = "restaurants";  //Name der Datenbank
+                        
+                        // Verbindung zur Datenbank herstellen
+                        $connection = new mysqli($servername, $username, $password, $database);
+                        
+                        // Überprüfen, ob die Verbindung erfolgreich hergestellt wurde
+                        if ($connection->connect_error) {
+                            die("Verbindung zur Datenbank fehlgeschlagen: " . $connection->connect_error);
+                        }
 
-                $sql = "SELECT * FROM restaurants_all";
+                        $sql = "SELECT * FROM restaurants_all";
 
-                // Die Abfrage ausführen und das Ergebnis speichern
-                $result = $connection->query($sql);
+                        // Die Abfrage ausführen und das Ergebnis speichern
+                        $result = $connection->query($sql);
 
-                // Überprüfen, ob die Abfrage erfolgreich war
-                if ($result) {
-                    // Daten der Db als Tabelle ausgeben und auf der Webseite anzeigen
-                    while ($row = $result->fetch_assoc()) {
-                        echo "<div class='table_div'>";
-                        echo "<table class='choice_table'>";
-                        echo "<tr>";
-                        echo "<a href='google.com'>"."<td class='td restaurant_name'>"."Restaurant: " . $row["Restaurant_Name"]."</td>"."</a>";
-                        echo "</tr>";
-                        echo "<tr>";
-                        echo "<td class='td restaurant_min_order'>"."Mindestbestellwert: ". $row["Mindestbestellwert"]. " €"."</td>";
-                        echo "<td class='td restaurant_time_order'>"."Lieferzeit: ca. ". $row["Lieferzeit"]. "min."."</td>";
-                        echo "</tr>";
-                        echo "<tr>";
-                        echo "<td class='td restaurant_price_order'>"."Lieferkosten: " . $row["Lieferkosten"]." €"."</td>";
-                        echo "<td class='td restaurant_typ_order'>"."Typ: " . $row["Typ"]."</td>";
-                        echo "</tr>";
-                        echo "<tr>";
-                        echo "<td class='td restaurant_categorie_order'>"."Kategorie: " . $row["Kategorie"]."</td>";
-                        echo "<td class='td restaurant_price'>"."Preis: " . $row["Preis"]."</td>"."</br>";
-                        echo "</tr>";
-                        echo "</table>";
-                        echo "</div>";
-                    }
+                        // Überprüfen, ob die Abfrage erfolgreich war
+                        if ($result) {
+                            // Daten der Db als Tabelle ausgeben und auf der Webseite anzeigen
+                            while ($row = $result->fetch_assoc()) {
+                                echo "<div class='choice_div'>";
+                                    echo "<table class='choice_table'>";
+                                    echo "<tr>";
+                                    echo "<td class='td restaurant_name'>"."Restaurant: " ."<a href='LukasGRIDTEST.html?Restaurant=$row[Restaurant_Name]'>". $row["Restaurant_Name"]."</a>"."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td class='td restaurant_min_order'>"."Mindestbestellwert: ". $row["Mindestbestellwert"]. " €"."</td>";
+                                    echo "<td class='td restaurant_time_order'>"."Lieferzeit: ca. ". $row["Lieferzeit"]. "min."."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td class='td restaurant_price_order'>"."Lieferkosten: " . $row["Lieferkosten"]." €"."</td>";
+                                    echo "<td class='td restaurant_typ_order'>"."Typ: " . $row["Typ"]."</td>";
+                                    echo "</tr>";
+                                    echo "<tr>";
+                                    echo "<td class='td restaurant_categorie_order'>"."Kategorie: " . $row["Kategorie"]."</td>";
+                                    echo "<td class='td restaurant_price'>"."Preis: " . $row["Preis"]."</td>"."</br>";
+                                    echo "</tr>";
+                                    echo "</table>";
+                                echo "</div>";
+                            }
 
-                    // Verbindung schließen
-                    $connection->close();
-                } else {
-                    echo "Fehler bei der Abfrage: " . $connection->error;
-                }
-            ?>
+                            // Verbindung schließen
+                            $connection->close();
+                        } else {
+                            echo "Fehler bei der Abfrage: " . $connection->error;
+                        }
+                    ?> 
+                </div>
             </div>
         </div>
     </body>
