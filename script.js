80bfdd27906a4e339715dc7406883b8250ce02ed
Max <maxwiese15.1@gmx.de>
2023-11-15 22:12:34 +0100

    The changes made in this commit are: - The loadCSV() function has been removed. - The HTML code for the table has been removed. - The script.js file has been reduced to a single line that declares an empty array named "choice". (generated by useblackbox.ai )

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-function loadCSV() {                                                                /* Erstellt eine Funktion mit dem namen loadCSV*/
-
-    var csvFile = "/Datenbanken/restaurants_all.csv";                            /* Erstellt eine Variable mit dem Namen csvFile. 
-                                                                                    Diese Variable speichert den Ablageort der zu verwendenden CSV */
-
-    var table = document.getElementById("csvTable");                                /* Erstellt eine Variable mit dem Namen table. 
-                                                                                    Diese Variable sucht in der HTML den bereich mit der ID="csvTable"*/
-    var xhttp = new XMLHttpRequest();
-
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                var lines = this.responseText.split("\n");
-                for (var i = 0; i < lines.length; i++) {
-                    var row = table.insertRow(-1);
-                    var cells = lines[i].split(",");
-                    for (var j = 0; j < cells.length; j++) {
-                        var cell = row.insertCell(-1);
-                        cell.innerHTML = cells[j];
-                    }
-                }
-            } else {
-                console.error("Failed to load CSV. Status code: " + this.status);
-            }
-        }else {
-            console.error("Failed to load CSV. Status code: " + this.status);
-        }
-    };
-
-    xhttp.open("GET", csvFile, true);
-    xhttp.send();
-}
 //----------- Die Auswahl wird in einem Array("coice") gespeichert -----------
 var choice = []
     
