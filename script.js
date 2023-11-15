b0e7b5bb43b8bcf749f213391bac873200523199
Max <maxwiese15.1@gmx.de>
2023-11-15 12:27:13 +0100

    Added functionality to display a shopping cart and save its contents to a cookie. (generated by useblackbox.ai )

--- script.js
+++ script.js
@@ -177,3 +177,130 @@ function submit_choice(){
 
     window.open("index.html", "_blank"); //----------- 1. Wert ist das ziel, 2. Wert ist der Ort------- um ein neuen Tab zu öffnen, wir nicht "_self" sondern "_blank" benutzt-----------
 }
+// Dieser Code wird dazu verwendet, einen Warenkorb funktional darzustellen.
+var warenkorbPreis = 0;
+var warenkorb = {
+  produkte: [],  // Array für die Namen der Produkte im Warenkorb
+  menge: [],     // Array für die Menge jedes Produkts im Warenkorb
+  preis: []      // Array für den Preis jedes Produkts im Warenkorb
+};
+var cookieStr = ''; // Initialisierung vom Cookie String
+var expireTime = new Date(); // Erstellung einer endzeit des Cookies (24h)
+expireTime = new Date(a.getTime() +1000*60*60*24);
+
+//checke Cookies und ausgabe des Warenkorbs
+function warenkorbAnzeigenOnload(){
+  checkCookie();
+  warenkorbAnzeigen();
+}
+
+//Hinzufügen von Produkten in den Warenkorb
+function zumWarenkorb(nr, pr) {
+  var produktName = document.getElementById('produkt' + nr).innerHTML;
+  var preis = pr;
+
+  // Sucht nach dem Produkt im Warenkorb
+  var index = warenkorb.produkte.indexOf(produktName);
+
+  if (index === -1) {
+    warenkorb.produkte.push(produktName);
+    warenkorb.menge.push(1);
+    warenkorb.preis.push(preis);
+  } else {
+    warenkorb.menge[index] += 1;
+  }
+
+  cookieSave();
+  warenkorbAnzeigen();
+}
+
+//Ausgeben des Warenkorbs in form von <artikels> 
+function warenkorbAnzeigen() {
+  warenkorbSum();  // Warenkorb-Gesamtpreis berechnen
+  var ausgabe = '<article class="warenkorbArtikel">' + "Bestellung:" + '</article>';
+
+  for (var i = 0; i < warenkorb.produkte.length; i++) {
+    ausgabe += '<article class="warenkorbArtikel">';
+    ausgabe += '<table><tr><td class="warenkorbTabelleZellen">' + warenkorb.menge[i] + 'x </td><td class="warenkorbTabelleItem">' + warenkorb.produkte[i] + ': </td><td class="warenkorbTabellePrice"> ' + (warenkorb.preis[i] / 100) + '&#x20AC </td><td class="warenkorbTabelleZellen">';
+    ausgabe += '<button class="warenkorbButtonLöschen" onclick="loescheProdukt(' + i + ');"><img src="pictures/delete.png" alt="delete"></button></td></tr></table>';
+    ausgabe += '<br>';
+    ausgabe += '</article>';
+  }
+
+  ausgabe += "Summe: " + warenkorbPreis/100 + "€";
+  ausgabe += '<br>';
+  ausgabe += '<br>';
+
+  document.getElementById('waren').innerHTML = ausgabe;
+
+  cookieSave();
+
+  warenkorbPreis = 0;
+}
+
+// Löschen von Produkten aus dem Warenkorb
+function loescheProdukt(index) {
+  if (warenkorb.menge[index] > 1) {
+    warenkorb.menge[index] -= 1;
+  } else {
+    warenkorb.produkte.splice(index, 1);
+    warenkorb.menge.splice(index, 1);
+    warenkorb.preis.splice(index, 1);
+  }
+  warenkorbAnzeigen();
+}
+
+//Zusammenrechnen der Warenkorbsumme
+function warenkorbSum(){
+  // Schleife durch alle Produkte im Warenkorb
+  for (var i = 0; i < warenkorb.produkte.length; i++) {
+    // Gesamtpreis berechnen, indem Menge und Preis multipliziert und zur Gesamtsumme hinzugefügt werden
+    warenkorbPreis += warenkorb.menge[i] * warenkorb.preis[i];
+  }
+}
+
+// Der check ob ein Cookie existiert und das Extrahieren seiner daten 
+function checkCookie() {
+  var cookieList = (document.cookie) ? document.cookie.split(';') : [];
+  var cookieValues = {};
+
+
+
+
+if(document.cookie){
+
+    const decodedCookie = decodeURIComponent(document.cookie); // entfernen der Cookieinformationen 
+    const cuttedString = decodedCookie.split('Warenkorb=').pop().split(';')[0]; //Inhalt unseres Cookies vom Namen und von Weiteren Cookies Trennen
+    const cuttedStringWithoutSpaces = cuttedString.trim(); //Lehrzeichen entfernen
+    const array2 = cuttedStringWithoutSpaces.split(",").filter(item => item !== ""); //Einzelne Cookieinformationen Trennen und leere Felder Löschen 
+
+    // Schreiben der gespeicherten Informationen in den Warenkorb
+    for (let i = 0; i < array2.length; i += 3) {
+      warenkorb.produkte.push(array2[i]);
+      warenkorb.menge.push(Number(array2[i + 1]));
+      warenkorb.preis.push(Number(array2[i + 2]));
+    }
+}
+}
+
+
+//Cookie mit den Warenkorbinformationen speichern
+function cookieSave(){
+  for(var i = 0; i < warenkorb.produkte.length; i++) {
+    cookieStr += warenkorb.produkte[i]+',';
+    cookieStr += warenkorb.menge[i]+',';
+    cookieStr += warenkorb.preis[i]+',';
+  }
+  //Cookie Speichern
+  document.cookie = 'Warenkorb='+cookieStr+'; expires='+expireTime+';';
+  cookieStr = ''; //Leeren des CookieStings
+}
+
+
+//DARKMODE//
+function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");}
+
+
+
