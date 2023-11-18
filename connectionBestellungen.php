2193852f0c66c712e8c139ceda93ce345437e385
RamsesTuerk <ramses.tuerk@iu-study.org>
2023-11-18 17:18:52 +0100

    Einbauen von Ajax Redundanzen entfernen

---
+++ connectionBestellungen.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$servername = "localhost";
+$user = "root";
+$password = "";
+$db = "bestellungen";
+
+$connection = new mysqli($servername, $user, $password, $db);
+
+if ($connection->connect_error) {
+    die("FAILED " . $connection->connect_error);
+}
+?>
+</body>
+</html>
---
+++ getBestellungen.php
@@ -0,0 +1,32 @@
+<?php
+require("connectionBestellungen.php");
+
+$restaurant_name="waybackburgers";
+
+
+$sql = "SELECT * FROM orders_$restaurant_name";
+    
+$result = $connection->query($sql);
+
+$data=array();
+
+    // Überprüfen, ob Daten vorhanden sind, bevor json_encode aufgerufen wird
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $data[] = $row;
+   }
+}
+
+    // Setzen Sie den Content-Type-Header auf application/json
+ header('Content-Type: application/json');
+
+
+    // Verwenden Sie json_encode nur, wenn Daten vorhanden sind
+ if (!empty($data)) {
+     echo json_encode($data);
+} else {
+     echo json_encode(array("message" => "No products found"));
+}
+
+$connection->close();
+?>
