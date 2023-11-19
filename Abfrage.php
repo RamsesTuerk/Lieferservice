e8df16cd8e2685d21d1c61462ee5945570dd3ccd
Max <maxwiese15.1@gmx.de>
2023-11-19 17:41:07 +0100

    The commit adds HTML, CSS, and JavaScript files to the project. It also adds a PHP script that connects to a database and checks if a user's login credentials are correct. If the credentials are correct, the user is logged in and taken to a page where they can see their orders. (generated by blackbox.ai )

--- Abfrage.php
+++ Abfrage.php
@@ -1,14 +1,35 @@
-</head>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="style.css"> <!--Verbindet die Webseite mit der CSS-Datei "Style.css"-->
+        <title>Burger, Pizza, Sushi</title> <!--Legt den Titel der Seite fest-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+        <script language="javascript" type="text/javascript" src="script.js"></script> <!--Verbindet die Webseite mit der JS-Datei-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" charset="utf-8">
+        <script type="text/javascript">
+        </script>
+
+    </head>
     <body>
     <?php
+    $restaurant_name = strtolower($_POST['restaurant_name']);
 
+    echo '<script>';
+        echo 'var restaurant_name ="' . $restaurant_name.'";';
+    echo '</script>';
     if ($_SERVER["REQUEST_METHOD"] == "POST") 
 
      require("connectionBenutzer.php");
-	
+
+
+    $restaurant_name = $_POST["restaurant_name"];
+    $pw = $_POST["pw"];
+
+    $sql = "SELECT * FROM benutzer WHERE restaurant_name = '$restaurant_name' AND pw = '$pw'";
     $result = $connection->query($sql);
 
     if ($result->num_rows > 0) {
+
 
     } else {
         echo "Login fehlgeschlagen. Benutzername oder Passwort ist falsch.";
@@ -19,3 +40,7 @@
     echo '<script>';
         echo 'var restaurant_name ="' . $restaurant_name.'";';
     echo '</script>';
+    ?>
+    <div id="Bestellungen"></div>
+    </body>
+</html>
---
+++ Datenbanken/survey_2.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 19. Nov 2023 um 16:55
+-- Server-Version: 10.4.28-MariaDB
+-- PHP-Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `survey`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `survey_questions`
+--
+
+CREATE TABLE `survey_questions` (
+  `id` int(11) NOT NULL,
+  `question1` varchar(255) NOT NULL,
+  `question2` varchar(255) NOT NULL,
+  `question3` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `survey_questions`
+--
+ALTER TABLE `survey_questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `survey_questions`
+--
+ALTER TABLE `survey_questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
